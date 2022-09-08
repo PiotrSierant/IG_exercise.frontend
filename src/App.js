c384184f30ccb55b097a3acbc2135bccca9c70af
PiotrSierant <piotr.sierant96@gmail.com>
2022-09-08 20:09:01 +0200

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
 
 function App() {
-  return <h1>zaczynamy :)</h1>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/IG_exercise.frontend" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
