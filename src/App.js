c6e30c5475dd5b04c3a21fafd82eec82a0f18967
PiotrSierant <piotr.sierant96@gmail.com>
2022-09-11 12:50:50 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar/Navbar";
 import { About } from "./components/About/About";
